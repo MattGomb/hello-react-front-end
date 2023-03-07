c21b278c95a53c5e280bd4a06c6e02a2bb352495
MattGomb <gombosonline@gmail.com>
2023-03-07 17:58:43 -0500

    Fix structure and logic and imports

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import './App.css';
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { Store } from '../redux/ConfigureStore';
-import Greeting from './Greeting';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
-    <Provider store={Store}>
-      <container>
-        <Routes>
-          <Route path="/" element={<Greeting />} />
-        </Routes>
-      </container>
-    </Provider>
+    <container>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </container>
   );
 }
 
